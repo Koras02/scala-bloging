2e7e80a4825820703748ce3f03f54facfe65b509
Koras02 <wjdgh0727@gmail.com>
2025-08-07 14:46:46 +0900

    Main

---
+++ src/main/scala/Hello.scala
@@ -0,0 +1,9 @@
+object HelloScala {
+    def main(args: Array[String]): Unit = {
+        println("Hello, Scala!");
+
+        val a = 10
+        val b = 20 
+        println(s"Sum of $a and $b is ${a + b}")
+    }
+}
